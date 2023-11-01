d56cb2f99a952193d15dd17d230193dbb4acef23
Princic-1837592 <andreapri.99@gmail.com>
2023-11-01 17:38:07 +0100

    primo commit

---
+++ chain-reaction/src/lib.rs
@@ -0,0 +1,154 @@
+use std::collections::VecDeque;
+
+#[cfg(feature = "serde")]
+use serde::Serialize;
+
+#[cfg(test)]
+mod tests;
+
+#[derive(Clone, Debug)]
+#[cfg_attr(feature = "serde", derive(Serialize))]
+pub struct Game {
+    board: Vec<Vec<Cell>>,
+    players: Vec<Player>,
+    turn: usize,
+    atoms: usize,
+    max_atoms: usize,
+}
+
+#[derive(Copy, Clone, Debug)]
+#[cfg_attr(feature = "serde", derive(Serialize))]
+#[cfg_attr(test, derive(PartialEq, Eq))]
+struct Cell {
+    atoms: usize,
+    player: usize,
+    max_atoms: usize,
+}
+
+#[derive(Copy, Clone, Debug, Default)]
+#[cfg_attr(feature = "serde", derive(Serialize))]
+struct Player {
+    atoms: usize,
+}
+
+type Coord = (usize, usize);
+
+impl Game {
+    pub fn new(height: usize, width: usize, players: usize) -> Self {
+        let mut board = vec![vec![Cell::default(); width]; height];
+        for (r, row) in board.iter_mut().enumerate() {
+            for (c, cell) in row.iter_mut().enumerate() {
+                cell.max_atoms = Self::max_atoms((r, c), height, width);
+            }
+        }
+        Self {
+            board,
+            players: vec![Player::default(); players],
+            turn: 0,
+            atoms: 0,
+            max_atoms: 4
+                + 2 * ((height - 2) * 2 + (width - 2) * 2)
+                + 3 * ((height - 2) * (width - 2)),
+        }
+    }
+
+    const fn max_atoms((row, col): Coord, height: usize, width: usize) -> usize {
+        let is_horizontal_edge = row == 0 || row == height - 1;
+        let is_vertical_edge = col == 0 || col == width - 1;
+        if is_horizontal_edge && is_vertical_edge {
+            // angolo
+            2
+        } else if is_horizontal_edge || is_vertical_edge {
+            // bordo
+            3
+        } else {
+            // centro
+            4
+        }
+    }
+
+    fn next_turn(&mut self) {
+        loop {
+            self.turn = (self.turn + 1) % self.players.len();
+            // se ci sono meno atomi del numero di giocatori significa che nessuno può essere stato
+            // eliminato quindi si può passare al turno successivo
+            // se invece il giocatore successivo non è stato eliminato tocca a lui
+            if self.atoms <= self.players.len() || self.players[self.turn].atoms > 0 {
+                break;
+            }
+        }
+    }
+
+    pub fn add_atom(&mut self, coord @ (row, col): Coord) -> bool {
+        let cell = &mut self.board[row][col];
+        // se la cella è già occupata
+        if cell.atoms != 0 && cell.player != self.turn {
+            return false;
+        }
+        // se la scacchiera è piena si andrebbe in un loop infinito
+        if self.atoms == self.max_atoms {
+            return false;
+        }
+        cell.player = self.turn;
+        self.players[self.turn].atoms += 1;
+        self.atoms += 1;
+        if cell.is_unstable() {
+            self.explode(coord);
+        } else {
+            cell.atoms += 1;
+        }
+        self.next_turn();
+        true
+    }
+
+    fn explode(&mut self, coord: Coord) {
+        let mut queue = VecDeque::from([coord]);
+        while let Some((row, col)) = queue.pop_front() {
+            let cell = &mut self.board[row][col];
+            if cell.atoms != 0 && cell.player != self.turn {
+                self.players[cell.player].atoms -= cell.atoms;
+                self.players[self.turn].atoms += cell.atoms;
+            }
+            cell.player = self.turn;
+            cell.atoms += 1;
+            if cell.must_explode() {
+                cell.atoms -= cell.max_atoms;
+                if cell.atoms == 0 {
+                    cell.player = usize::MAX;
+                }
+                if row > 0 {
+                    queue.push_back((row - 1, col));
+                }
+                if row < self.board.len() - 1 {
+                    queue.push_back((row + 1, col));
+                }
+                if col > 0 {
+                    queue.push_back((row, col - 1));
+                }
+                if col < self.board[0].len() - 1 {
+                    queue.push_back((row, col + 1));
+                }
+            }
+        }
+    }
+}
+
+impl Cell {
+    const fn is_unstable(&self) -> bool {
+        self.atoms + 1 == self.max_atoms
+    }
+
+    const fn must_explode(&self) -> bool {
+        self.atoms >= self.max_atoms
+    }
+}
+
+impl Default for Cell {
+    fn default() -> Self {
+        Self {
+            atoms: 0,
+            player: usize::MAX,
+            max_atoms: 0,
+        }
+    }
+}
---
+++ chain-reaction/src/tests.rs
@@ -0,0 +1,416 @@
+use crate::{Cell, Game};
+
+#[test]
+fn max_atoms() {
+    let game = Game::new(2, 2, 4);
+    assert_eq!(game.max_atoms, 4);
+    let game = Game::new(3, 3, 4);
+    assert_eq!(game.max_atoms, 4 + 8 + 3);
+    let game = Game::new(4, 4, 4);
+    assert_eq!(game.max_atoms, 4 + 16 + 12);
+}
+
+#[test]
+fn max_atoms_cell() {
+    fn test(height: usize, width: usize) {
+        let game = Game::new(height, width, 4);
+        // prima controlla manualmente gli angoli
+        assert_eq!(game.board[0][0].max_atoms, 2);
+        assert_eq!(game.board[0][width - 1].max_atoms, 2);
+        assert_eq!(game.board[height - 1][0].max_atoms, 2);
+        assert_eq!(game.board[height - 1][width - 1].max_atoms, 2);
+        // poi controlla i bordi orizzontali
+        for i in 1..width - 1 {
+            assert_eq!(game.board[0][i].max_atoms, 3);
+            assert_eq!(game.board[height - 1][i].max_atoms, 3);
+        }
+        // poi controlla i bordi verticali
+        for i in 1..height - 1 {
+            assert_eq!(game.board[i][0].max_atoms, 3);
+            assert_eq!(game.board[i][width - 1].max_atoms, 3);
+        }
+        // infine controlla il centro
+        for i in 1..height - 1 {
+            for j in 1..width - 1 {
+                assert_eq!(game.board[i][j].max_atoms, 4);
+            }
+        }
+    }
+    test(5, 5);
+    test(10, 10);
+    test(100, 50);
+}
+
+#[test]
+fn next_turn() {
+    let mut game = Game::new(5, 5, 4);
+    // scorre tutti i giocatori, piazzando manualmente un atomo per ogni giocatore
+    // e fa andare avanti il turno
+    // l'atomo non viene piazzato davvero ma solo aumentato il contatore globale
+    for t in 0..4 {
+        assert_eq!(game.turn, t);
+        game.atoms += 1;
+        game.next_turn();
+    }
+    assert_eq!(game.turn, 0);
+    // aumenta il contatore di atomi di due giocatori ed aggiusta quello globale
+    // così che i giocatori 1 e 3 risultino eliminati
+    game.players[0].atoms = 3;
+    game.players[2].atoms = 3;
+    game.atoms = 6;
+    game.next_turn();
+    assert_eq!(game.turn, 2);
+    game.next_turn();
+    assert_eq!(game.turn, 0);
+}
+
+#[test]
+fn add_atom() {
+    let mut game = Game::new(5, 5, 2);
+    game.add_atom((0, 0));
+    /*
+    1 0 0 0 0
+    0 0 0 0 0
+    0 0 0 0 0
+    0 0 0 0 0
+    0 0 0 0 0
+    */
+    assert_eq!(
+        game.board[0][0],
+        Cell {
+            atoms: 1,
+            player: 0,
+            max_atoms: 2,
+        }
+    );
+    assert_eq!(game.players[0].atoms, 1);
+    assert_eq!(game.atoms, 1);
+
+    // verifica che non si possa muovere dove è già occupato da un altro giocatore
+    assert!(!game.add_atom((0, 0)));
+
+    game.add_atom((4, 4));
+    /*
+    1 0 0 0 0
+    0 0 0 0 0
+    0 0 0 0 0
+    0 0 0 0 0
+    0 0 0 0 1
+    */
+    assert_eq!(
+        game.board[4][4],
+        Cell {
+            atoms: 1,
+            player: 1,
+            max_atoms: 2,
+        }
+    );
+    assert_eq!(game.players[1].atoms, 1);
+    assert_eq!(game.atoms, 2);
+
+    // verifica che non si possa muovere dove è già occupato da un altro giocatore
+    assert!(!game.add_atom((4, 4)));
+
+    game.add_atom((0, 0));
+    /*
+    0 1 0 0 0
+    1 0 0 0 0
+    0 0 0 0 0
+    0 0 0 0 0
+    0 0 0 0 1
+    */
+    assert_eq!(
+        game.board[0][0],
+        Cell {
+            atoms: 0,
+            player: usize::MAX,
+            max_atoms: 2,
+        }
+    );
+    assert_eq!(
+        game.board[0][1],
+        Cell {
+            atoms: 1,
+            player: 0,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(
+        game.board[1][0],
+        Cell {
+            atoms: 1,
+            player: 0,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(game.players[0].atoms, 2);
+    assert_eq!(game.atoms, 3);
+
+    game.add_atom((4, 4));
+    /*
+    0 1 0 0 0
+    1 0 0 0 0
+    0 0 0 0 0
+    0 0 0 0 1
+    0 0 0 1 0
+    */
+    assert_eq!(
+        game.board[4][4],
+        Cell {
+            atoms: 0,
+            player: usize::MAX,
+            max_atoms: 2,
+        }
+    );
+    assert_eq!(
+        game.board[4][3],
+        Cell {
+            atoms: 1,
+            player: 1,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(
+        game.board[3][4],
+        Cell {
+            atoms: 1,
+            player: 1,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(game.players[1].atoms, 2);
+    assert_eq!(game.atoms, 4);
+
+    game.add_atom((0, 0));
+    game.add_atom((4, 4));
+    /*
+    1 1 0 0 0
+    1 0 0 0 0
+    0 0 0 0 0
+    0 0 0 0 1
+    0 0 0 1 1
+    */
+
+    game.add_atom((0, 0));
+    /*
+    0 2 0 0 0
+    2 0 0 0 0
+    0 0 0 0 0
+    0 0 0 0 1
+    0 0 0 1 1
+    */
+    assert_eq!(
+        game.board[0][0],
+        Cell {
+            atoms: 0,
+            player: usize::MAX,
+            max_atoms: 2,
+        }
+    );
+    assert_eq!(
+        game.board[0][1],
+        Cell {
+            atoms: 2,
+            player: 0,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(
+        game.board[1][0],
+        Cell {
+            atoms: 2,
+            player: 0,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(game.players[0].atoms, 4);
+    assert_eq!(game.atoms, 7);
+
+    game.add_atom((4, 4));
+    /*
+    0 2 0 0 0
+    2 0 0 0 0
+    0 0 0 0 0
+    0 0 0 0 2
+    0 0 0 2 0
+    */
+    assert_eq!(
+        game.board[4][4],
+        Cell {
+            atoms: 0,
+            player: usize::MAX,
+            max_atoms: 2,
+        }
+    );
+    assert_eq!(
+        game.board[4][3],
+        Cell {
+            atoms: 2,
+            player: 1,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(
+        game.board[3][4],
+        Cell {
+            atoms: 2,
+            player: 1,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(game.players[1].atoms, 4);
+    assert_eq!(game.atoms, 8);
+
+    game.add_atom((0, 0));
+    game.add_atom((4, 4));
+    /*
+    1 2 0 0 0
+    2 0 0 0 0
+    0 0 0 0 0
+    0 0 0 0 2
+    0 0 0 2 1
+    */
+
+    game.add_atom((0, 0));
+    /*
+    0 1 1 0 0
+    1 2 0 0 0
+    1 0 0 0 0
+    0 0 0 0 2
+    0 0 0 2 1
+    */
+    assert_eq!(
+        game.board[0][0],
+        Cell {
+            atoms: 0,
+            player: usize::MAX,
+            max_atoms: 2,
+        }
+    );
+    assert_eq!(
+        game.board[0][1],
+        Cell {
+            atoms: 1,
+            player: 0,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(
+        game.board[1][0],
+        Cell {
+            atoms: 1,
+            player: 0,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(
+        game.board[0][2],
+        Cell {
+            atoms: 1,
+            player: 0,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(
+        game.board[1][1],
+        Cell {
+            atoms: 2,
+            player: 0,
+            max_atoms: 4,
+        }
+    );
+    assert_eq!(
+        game.board[2][0],
+        Cell {
+            atoms: 1,
+            player: 0,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(game.players[0].atoms, 6);
+    assert_eq!(game.atoms, 11);
+
+    game.add_atom((4, 4));
+    /*
+    0 1 1 0 0
+    1 2 0 0 0
+    1 0 0 0 1
+    0 0 0 2 1
+    0 0 1 1 0
+    */
+    assert_eq!(
+        game.board[4][4],
+        Cell {
+            atoms: 0,
+            player: usize::MAX,
+            max_atoms: 2,
+        }
+    );
+    assert_eq!(
+        game.board[4][3],
+        Cell {
+            atoms: 1,
+            player: 1,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(
+        game.board[3][4],
+        Cell {
+            atoms: 1,
+            player: 1,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(
+        game.board[4][2],
+        Cell {
+            atoms: 1,
+            player: 1,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(
+        game.board[3][3],
+        Cell {
+            atoms: 2,
+            player: 1,
+            max_atoms: 4,
+        }
+    );
+    assert_eq!(
+        game.board[2][4],
+        Cell {
+            atoms: 1,
+            player: 1,
+            max_atoms: 3,
+        }
+    );
+    assert_eq!(game.players[1].atoms, 6);
+    assert_eq!(game.atoms, 12);
+}
+
+#[test]
+fn elimination() {
+    let mut game = Game::new(2, 2, 2);
+    assert_eq!(game.turn, 0);
+    game.add_atom((0, 0));
+    assert_eq!(game.turn, 1);
+    game.add_atom((0, 1));
+    assert_eq!(game.turn, 0);
+    game.add_atom((0, 0));
+    assert_eq!(game.turn, 0);
+}
+
+#[test]
+fn no_more_space() {
+    let mut game = Game::new(2, 2, 2);
+    assert!(game.add_atom((0, 0)));
+    assert!(game.add_atom((0, 1)));
+    assert!(game.add_atom((1, 0)));
+    assert!(game.add_atom((1, 1)));
+    assert!(!game.add_atom((0, 0)));
+    assert!(!game.add_atom((0, 1)));
+    assert!(!game.add_atom((1, 0)));
+    assert!(!game.add_atom((1, 1)));
+}
