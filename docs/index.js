96701554aefedcb6a151fe68431f8506d494c619
Princic-1837592 <andreapri.99@gmail.com>
2023-11-02 12:19:06 +0100

    bozza sito mobile e desktop

--- docs/index.js
+++ docs/index.js
@@ -2,24 +2,47 @@ import init, {} from "./pkg/frontend.js";
 
 init().then(initializePage);
 
-const FORCE_MOBILE = false;
+const FORCE_MOBILE = true;
+const SMALL_WIDTH = 6, SMALL_HEIGHT = 11;
+const BIG_WIDTH = 10, BIG_HEIGHT = 18;
 
 function initializePage() {
-    let dir = "desktop";
+    let device = "desktop";
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || FORCE_MOBILE) {
-        dir = "mobile";
+        device = "mobile";
     }
-    for (const style of ["big", "selection", "small"]) {
-        const css = document.createElement("link");
-        css.rel = "stylesheet";
-        css.href = `${dir}/${style}.css`
-        document.getElementsByTagName("body")[0].appendChild(css);
+    document.getElementById("grid-container").classList.add(device);
+    //todo togliere
+    initializeGrid("big");
+    render();
+}
+
+function initializeGrid(type) {
+    const grid = document.getElementById("grid");
+    let width;
+    let height;
+    if (type === "small") {
+        width = SMALL_WIDTH;
+        height = SMALL_HEIGHT;
+        grid.classList.add("small");
+    } else {
+        width = BIG_WIDTH;
+        height = BIG_HEIGHT;
+        grid.classList.add("big");
+    }
+    const row_template = document.createElement("div");
+    row_template.classList.add("row");
+    const cell_template = document.createElement("div");
+    cell_template.classList.add("cell")
+    for (let i = 0; i < height; i++) {
+        const row = row_template.cloneNode(false);
+        for (let j = 0; j < width; j++) {
+            row.appendChild(cell_template.cloneNode(false));
+        }
+        grid.appendChild(row);
     }
 }
 
-function initializeGrid() {
-    const container = document.getElementById("grid-container");
-    const width = 6, height = 8;
-    const cell = document.createElement("div");
-    cell.classList.add("cell")
+function render() {
+    
 }
