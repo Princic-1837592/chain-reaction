318867e59f9b72c06345c78e5f03d53383131454
Princic-1837592 <andreapri.99@gmail.com>
2023-11-02 14:22:39 +0100

    `new_game` e `get_state` wasm

---
+++ docs/pkg/frontend.d.ts
@@ -0,0 +1,51 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} height
+* @param {number} width
+* @param {number} players
+* @returns {boolean}
+*/
+export function newGame(height: number, width: number, players: number): boolean;
+/**
+* @param {number} _row
+* @param {number} _column
+* @returns {string | undefined}
+*/
+export function addAtom(_row: number, _column: number): string | undefined;
+/**
+* @returns {string}
+*/
+export function getState(): string;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly newGame: (a: number, b: number, c: number) => number;
+  readonly addAtom: (a: number, b: number, c: number) => void;
+  readonly getState: (a: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
--- docs/wasm/src/lib.rs
+++ docs/wasm/src/lib.rs
@@ -1,24 +1,30 @@
 use std::cell::RefCell;
 
 use chain_reaction::Game;
-use serde_json::{to_string, Number, Value};
+use serde_json::to_string;
 use wasm_bindgen::prelude::wasm_bindgen;
 
 thread_local! {
-static GAME: RefCell<Game> = RefCell::new(Game::new(0, 0, 0));
+static GAME: RefCell<Game> = RefCell::new(Game::new(3, 3, 2).unwrap());
 }
 
-// #[wasm_bindgen(js_name = "newGame")]
-pub fn new_game(height: usize, width: usize, max_history: usize, seed: String) -> String {
-    unimplemented!()
+#[wasm_bindgen(js_name = "newGame")]
+pub fn new_game(height: usize, width: usize, players: usize) -> bool {
+    let new_game = Game::new(height, width, players);
+    if let Some(game) = new_game {
+        GAME.with(|g| g.replace(game));
+        true
+    } else {
+        false
+    }
 }
 
-// #[wasm_bindgen(js_name = "addAtom")]
-pub fn add_atom(row: usize, column: usize) -> Option<String> {
+#[wasm_bindgen(js_name = "addAtom")]
+pub fn add_atom(_row: usize, _column: usize) -> Option<String> {
     unimplemented!();
 }
 
-// #[wasm_bindgen(js_name = "getState")]
+#[wasm_bindgen(js_name = "getState")]
 pub fn get_state() -> String {
-    unimplemented!();
+    GAME.with(|game| to_string(game).unwrap())
 }
